ce49bef42a07b1e9d851a51f24db94ac13a6ba1d
Elite Camerawoman <142908692+Kawaiichanbest@users.noreply.github.com>
2023-12-28 12:41:26 +0100

    Create Camerawoman.sh
    Camera woman is bot

---
+++ Camerawoman.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="Hi guys"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="You okay"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="I'm Camera woman"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="I Love you"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="<3"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="YAY"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
+
+BOT_TOKEN="MTE4OTg5MjcwOTcyNTUxMTc1MA.GVwWPw.Dp5Yq_o0YVP7M6qcjBWbhjyv2NPhTVuwYvlKBs"
+
+CHANNEL_ID="1189568402747052189"
+
+MESSAGE="I want hug?"
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bot $BOT_TOKEN" \
+-d "{\"content\":\"$MESSAGE\"}" \
+"https://discord.com/api/v10/channels/$CHANNEL_ID/messages"
