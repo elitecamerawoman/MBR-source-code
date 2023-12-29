0e76801c9a3ac13b35065a80405b0ace1a7f1c80
Elite Camerawoman <142908692+Kawaiichanbest@users.noreply.github.com>
2023-12-29 08:49:54 +0100

    Create Discord.sh

---
+++ Discord.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BOT_TOKEN="YOUR_DISCORD_BOT_TOKEN"
+
+while true; do
+    # Your logic here (replace with your desired functionality)
+    echo "Bot is starting"
+
+    # Sleep for a duration (adjust as needed)
+    sleep 60
+done
